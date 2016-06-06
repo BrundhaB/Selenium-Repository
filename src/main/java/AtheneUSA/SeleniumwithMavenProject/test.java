df73a0a409da04ccc4ef19c50341387e5545b83b
Brundha Balasubramanain <E71253@L-H0HY6R1.amerus.corp.tld>
2016-06-06 12:08:13 -0500

    Added new test class

---
+++ src/main/java/AtheneUSA/SeleniumwithMavenProject/test.java
@@ -0,0 +1,12 @@
+package AtheneUSA.SeleniumwithMavenProject;
+
+public class test {
+	
+	public static void main( String[] args )
+    {
+        System.out.println( "test" );
+        
+        
+    }
+
+}
